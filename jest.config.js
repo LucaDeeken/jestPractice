e2c4c80dfbef18f7d9cb7f98f0776c7ba25c4a00
Luca Deeken <luca.deeken@fuchs-edv.de>
2025-03-27 17:25:34 +0100

    chore: initial project setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: 'node', // oder 'jsdom' für Browser-Umgebung
+    verbose: true, // Zeigt detaillierte Testberichte an
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest', // Falls du Babel verwendest
+    },
+    testMatch: [
+      '**/?(*.)+(spec|test).js', // Sucht nach Testdateien im Format .test.js oder .spec.js
+    ],
+  };
+  
